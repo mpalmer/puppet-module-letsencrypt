6cb318563ef23fc678744103e777db1876a505e5
Matt Palmer <mpalmer@hezmatt.org>
2016-11-16 09:55:29 +1100

    Don't re-run acme-issue every Puppet run

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -1,8 +1,13 @@
 define letsencrypt::certificate($names) {
 	include letsencrypt::scripts
 
-	exec { "Issue certificate ${name}":
+	$names_array = maybe_split($names, '\s+')
+
+	$filename = $names_array[0]
+
+	exec { "Issue certificate ${filename}":
 		command => "/usr/local/bin/acme-issue $names",
+		creates => "/var/lib/letsencrypt/certs/${filename}.pem",
 		require => File["/usr/local/bin/acme-issue"],
 	}
 }
