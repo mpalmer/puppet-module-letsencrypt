16cdd100694b986d5ea7a327565ee78527d17f3f
Matt Palmer <mpalmer@hezmatt.org>
2018-08-29 17:02:02 +1000

    Correctly invoke acme-issue for multi-sAN certs

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -3,11 +3,12 @@ define letsencrypt::certificate($names) {
 	include letsencrypt::cronjobs
 
 	$names_array = maybe_split($names, '\s+')
+	$name_list = join($names_array, " ")
 
 	$filename = $names_array[0]
 
 	exec { "Issue certificate ${filename}":
-		command => "/usr/local/bin/acme-issue $names",
+		command => "/usr/local/bin/acme-issue ${name_list}",
 		creates => "/var/lib/letsencrypt/certs/${filename}.pem",
 		require => File["/usr/local/bin/acme-issue"],
 	}
